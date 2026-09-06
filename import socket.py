c3e4005597dbb2af4965b6e47e67e98dc668029d
zahiddabdul208-hue <zahiddabdul208@gmail.com>
2026-09-06 14:00:39 +0700

    Delete import socket.py

--- import socket.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-domain = input(https://smpit-abubakar.sch.id/)
-
-try:
-    ip = socket.gethostbyname(domain)
-    print(f"IP website: {ip}")
-except socket.gaierror:
-    print("Domain tidak ditemukan.")
